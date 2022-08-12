78bf3d2e782ae8f6d717dceda058a820ac125a70
David Reed <x@sys32.dev>
2022-08-11 21:24:42 -0400

    socks as optional example dependency, add chained socks example

---
+++ examples/createServerChained.mjs
@@ -0,0 +1,26 @@
+import { createProxyServer } from '../dist/index.js';
+import { SocksClient } from 'socks';
+
+const server = createProxyServer({
+	async connect(port, host) {
+		console.log(port, host);
+		// connect to TOR socks proxy
+		const { socket } = await SocksClient.createConnection({
+			proxy: {
+				host: '127.0.0.1',
+				port: 9050, // TOR daemon
+				type: 5,
+			},
+			command: 'connect',
+			destination: {
+				port,
+				host,
+			},
+		});
+
+		return socket;
+	},
+});
+
+// start listening!
+server.listen(1080);
